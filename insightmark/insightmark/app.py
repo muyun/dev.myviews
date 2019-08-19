b6763c548b115ae8e4199210b918ca88e73e0441
wenlzhao <wenlzhao@gmail.com>
2019-08-19 15:56:40 +0800

    add insightmark

--- insightmark/insightmark/app.py
+++ insightmark/insightmark/app.py
@@ -2,47 +2,31 @@ import os
 
 from flask import Flask
 
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'insightmark.sqlite'),
+)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'insightmark.sqlite'),
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+#from . import database
+import database
+database.init_app(app)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+#from .app import auth
+import auth
+app.register_blueprint(auth.bp)
 
-    #from . import database
-    import database
-    database.init_app(app)
-
-    #from .app import auth
-    import auth
-    app.register_blueprint(auth.bp)
-
-    #from .app import insightmark
-    import insightmark
-    app.register_blueprint(insightmark.bp)
-    app.add_url_rule('/', endpoint='index')
-
-    return app
+#from .app import insightmark
+import post
+app.register_blueprint(post.bp)
+app.add_url_rule('/', endpoint='index')
 
 if __name__ == '__main__':
     app.run()
