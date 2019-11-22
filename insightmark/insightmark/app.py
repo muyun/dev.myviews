fb417202d38ba7605668aa3db6a13016e6a4d63b
muyun <wenlzhao@gmail.com>
2019-11-22 19:15:47 +0800

    update

--- insightmark/insightmark/app.py
+++ insightmark/insightmark/app.py
@@ -1,14 +1,22 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
+
 app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'insightmark.sqlite'),
 )
 
+"""
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://uligqnruzxurss:b427d869c5bced80eb8b413707d25c0e15ad81dbad80fd809ec76de0a9dd39dc@ec2-174-129-252-252.compute-1.amazonaws.com:5432/d6op0ied1i4pua'
+"""
+
+db = SQLAlchemy(app)
 
 # a simple page that says hello
 @app.route('/hello')
