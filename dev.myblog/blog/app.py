edf55325005963e516c42c583b92ef5be9e61039
muyun <wenlzhao@gmail.com>
2021-02-19 18:17:18 +0800

    add

--- dev.myblog/blog/app.py
+++ dev.myblog/blog/app.py
@@ -1,10 +1,23 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+def app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE = os.path.join(app.instance_path, 'blog.sqlite'),
+    )
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/hello')
+    def hello():
+        return "Hello, World!"
+
+    return app
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dev.myblog/note.md
+++ dev.myblog/note.md
@@ -1,10 +1,22 @@
 
 #### note  
 
+##### structure is the key  
+* Modules 
+  - modular programming - separating code into parts holding related data and function   
+  - each module has its **own private symbol table**; 
+  thus a module creates a **separate namespace**     
+  - import <module_name>  -> only place <module_name> in the caller's symbole table  
+  - dir() can find out which names a module defines  
+
+* Packages  
+
+  
 
 #### deploy  
 * run  
-  > export FLASK_APP = app.py  
+  > export FLASK_APP=blog
+  > export FLASK_ENV=development  
   > flask run  
 
 #### reference
